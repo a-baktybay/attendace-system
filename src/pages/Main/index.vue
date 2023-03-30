3e00d81af6caa4493603e98e32ec940067ca3cc9
amanbek <a.baktybaya@aviata.me>
2023-03-30 22:47:58 +0600

    feat: add auth page

--- src/pages/Main/index.vue
+++ src/pages/Main/index.vue
@@ -1,6 +1,41 @@
 <template>
-  <div>
-    <h1 class="text-red-500">Hello!</h1>
+  <div class="flex justify-center items-center h-screen w-screen bg-gray-100">
+    <main class="flex w-[1200px] h-[800px] p-9 rounded-3xl bg-white">
+      <div class="flex items-center h-full">
+        <img 
+          src="@/assets/images/auth-page-img.png" 
+          alt=""
+          width="591"
+          height="590"
+        >
+      </div>
+      <div class="flex flex-col justify-center items-center h-full">
+        <img 
+          src="@/assets/images/happy-logo.png" 
+          alt=""
+          width="425"
+          height="159"
+        >
+        <div class="flex flex-col items-center space-y-8">
+          <input
+          type="text" 
+          placeholder="ID number"
+          class="p-4 w-80 bg-gray-100 rounded-lg placeholder:text-gray-500 focus:outline-none"
+          >
+          <input
+          type="password" 
+          placeholder="Password"
+          class="p-4 w-80 bg-gray-100 rounded-lg placeholder:text-gray-500 focus:outline-none"
+          >
+          <button
+            type="button"
+            class="self-end py-0.5 w-20 px-4 rounded-lg text-white bg-blue-800 "
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,65 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    './src/**/*.{html,js,vue,jsx}',
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    screens: {
+      xs: '320px',
+      sm: '600px',
+      md: '800px',
+      lg: '1200px'
+    },
+    colors: {
+      transparent: 'transparent',
+      white: '#FFFFFF',
+      black: {
+        DEFAULT: '#000000'
+      },
+      green: {
+        200: '#BBF0B6',
+        300: '#B8E4C7',
+        500: '#4EBC73'
+      },
+      gray: {
+        100: '#8F929724',
+        200: '#EAECED',
+        300: '#D1D1D6',
+        400: '#AEAEB2',
+        500: '#8E8E93',
+        700: '#636366',
+        800: '#3A3A3C',
+        900: '#0F0F0F'
+      },
+      red: {
+        500: '#FF3347',
+        700: '#DB2547'
+      },
+      blue: {
+        200: '#B6E2F0',
+        500: '#66BDFF',
+        800: '#00246A'
+      },
+      orange: {
+        500: '#FF9500'
+      }
+    },
+    extend: {
+      fontSize: {
+        '2xs': ['0.688rem', { lineHeight: '0.813rem' }],
+        xs: ['0.813rem', { lineHeight: '1.125rem' }],
+        sm: ['0.875rem', { lineHeight: '1.25rem' }],
+        base: ['1rem', { lineHeight: '1.375rem' }],
+        lg: ['1.125rem', { lineHeight: '1.5rem' }],
+        xl: ['1.375rem', { lineHeight: '1.75rem' }],
+        '2xl': ['1.75rem', { lineHeight: '2.125rem' }],
+        '3xl': ['2rem', { lineHeight: '2rem' }]
+      },
+      stroke: {
+        current: 'currentColor'
+      },
+      spacing: {
+        '1/7': '14.2857143%'
+      }
+    }
+  }
+};
